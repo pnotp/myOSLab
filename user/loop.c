3fd79fc8ddd28a01610299096349ca01eb055ec4
pnotp <2435770399@qq.com>
2024-10-19 15:30:41 +0800

    rr-final

--- user/loop.c
+++ user/loop.c
@@ -8,9 +8,11 @@ main(int argc, char *argv[])
         int data[8];
         int i,j,k;
         pid=fork();
+        pid = getpid();
         printf("pid: %d started \n", pid);
         for(i=0;i<2;i++)
         {
+            printf("pid: %d running \n", pid);
             for(j=0;j<1024;j++)
                 for(k=0;k<1024*64;k++)
                     data[k%8]=pid*k;
